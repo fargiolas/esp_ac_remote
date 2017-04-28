d6986e93d99b27e520cf4186210bd09ca08e757d
Filippo Argiolas <filippo.argiolas@gmail.com>
2017-04-28 09:03:10 +0200

    esp: add some comment here and there
    Still a lot missing

--- esp/acremote/user/ir_driver.c
+++ esp/acremote/user/ir_driver.c
@@ -1,3 +1,23 @@
+/*
+  IR led driver that replicates Samsung ARH-901 remote functionality
+
+  Copyright (C) 2017 Filippo Argiolas <filippo.argiolas@gmail.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #include "ets_sys.h"
 #include "gpio.h"
 #include "os_type.h"
@@ -8,14 +28,28 @@
 #include "utils.h"
 #include "queue.h"
 
+/* TIMINGS */
+
+/* 38 kHz modulation, ~26us periods, 13us half periods */
+/* Mark: signal oscillates up and down with 26us period for a given
+ * amout of time */
+/* Space: signal stays low for a given amount of time */
+#define TICK 13 /* 1000/38/2 */
+
+
+/*
+  Header: 3ms mark + 9ms space
+  Data: each bit starts with a 500us mark and its value is encoded in
+  the subsequent space length, long space is one short space is 0.
+
+  Transmission ends with a bit mark and line goes down for at least one 1-bit space
+ */
 #define HEADER_MARK_US  3000
 #define HEADER_SPACE_US 9000
 
 #define BIT_SPACE_0_US  500 /* 450-470 */
 #define BIT_SPACE_1_US 1500 /* 1450 */
 #define BIT_MARK_US       500 /* 570 */
-
-#define TICK 13 /* 1000/38/2 */
 
 #define _gpio_pin  4
 #define _gpio_func FUNC_GPIO4
@@ -45,20 +79,8 @@ typedef struct _CommandData {
 
 static CommandData *cur_data = NULL;
 
-/*
-  Bit bang GPIO pin for <len> us.
-  TICK is expected to be half a period of the carrier (38kHz) in
-  microseconds (1000/38/2).
-
-  Not perfect but the receiver seems tolerant enough. Also, timing
-  from the original remote wasn't perfect at all.
-
-  A bit worried about those delays, each command can get pretty
-  lengthy, around 100ms. Watchdog seems happy, but still...
- */
-
-static void
-mark (uint16_t len) {
+static void mark (uint16_t len)
+{
     uint16_t i;
     for(i=0; i<len/TICK/2; i++) {
         GPIO_OUTPUT_SET(_gpio_pin, 0);
@@ -69,14 +91,18 @@ mark (uint16_t len) {
 }
 
 
-static void
-space (uint16_t len) {
+static void space (uint16_t len)
+{
     GPIO_OUTPUT_SET(_gpio_pin, 0);
     os_delay_us(len);
 }
 
-void
-ir_send_cmd_simple (uint8_t *buf) {
+
+/* Simple bit banging, it apparently works. Watchdog doesn't bark, but
+ * given a single command is usually of the order of 100ms, it should
+ * and it probably will. */
+void ir_send_cmd_simple (uint8_t *buf)
+{
     uint8_t i,j;
 
     mark(HEADER_MARK_US);
@@ -97,17 +123,17 @@ ir_send_cmd_simple (uint8_t *buf) {
     mark(BIT_MARK_US);
 }
 
-static ICACHE_FLASH_ATTR
-uint8_t checksum (uint8_t *buf) {
-    /* 34 - ones sum */
-    uint8_t acc,i,j;
-    acc = 34;
-    for (i=0; i<7;i++)
-        for (j=0; j<8; j++) {
-            acc -= (buf[i] & 1<<j)>>j;
-        }
-    return acc;
-}
+
+
+/* Async command state machine */
+/*
+  Locks only while sending marks, usually around 500us, 3ms for
+  headers, I should probably use hardware timers for these too.
+
+  Spaces are proper hardware delays.
+
+  Commands are put in a queue and executed asynchronously
+*/
 
 void machine_func (void);
 
@@ -236,7 +262,23 @@ void ir_send_cmd (uint8_t *command) {
     ir_send_cmd_full (command, NULL, NULL);
 }
 
+
+
 /* move everything to a proper object with checks and all */
+
+static ICACHE_FLASH_ATTR
+uint8_t checksum (uint8_t *buf) {
+    /* 34 - ones sum */
+    uint8_t acc,i,j;
+    acc = 34;
+    for (i=0; i<7;i++)
+        for (j=0; j<8; j++) {
+            acc -= (buf[i] & 1<<j)>>j;
+        }
+    return acc;
+}
+
+
 void ICACHE_FLASH_ATTR
 build_command(uint8_t *cmd,
               uint8_t mode, uint8_t temperature,
